4ee3a3b72a69ef26014f1ba140ff2676b7d2f3cf
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-06-18 17:26:26 +0200

    Setting all classes and start try to do runner

---
+++ src/main/java/lucadipietro/U5_W1_D2/entities/Menu.java
@@ -0,0 +1,18 @@
+package lucadipietro.U5_W1_D2.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Menu {
+    private List<Pizza> pizze;
+    private List<Topping> toppings;
+    private List<Drink> drinks;
+}
